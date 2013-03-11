583798d3989d53ae69bca46753a994fb768ddac3
blal <blal.bachir@gmail.com>
2013-03-11 20:18:17 +0100

    commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 31.01.2013
+ *      Author: blal
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Programm Start"<<endl;
+
+
+	cout<<"Programm End"<<endl;
+
+	return 0;
+}
